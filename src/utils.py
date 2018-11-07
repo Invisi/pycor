d7ea79cf2ac7639716154eee5d2adaf3342da1d6
Yannick Linke <invisi@0x0f.net>
2018-11-07 10:01:09 +0100

    Added crash handler, fixed logging level

--- src/utils.py
+++ src/utils.py
@@ -17,6 +17,16 @@ def setup_logger(level=logging.DEBUG):
     log = logging.getLogger('PyCor')
     fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
 
+    def crash_handler(exc_type, value, tb):
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, value, tb)
+            return
+        log.critical('Uncaught exception', exc_info=(exc_type, value, tb))
+        input('Press return to exit.')
+        sys.exit(1)
+
+    sys.excepthook = crash_handler
+
     hldr = logging.handlers.TimedRotatingFileHandler('logs/PyCor.log', when='W0', encoding='utf-8', backupCount=16)
     hldr.setFormatter(fmt)
     hldr.setLevel(logging.DEBUG)
@@ -25,6 +35,7 @@ def setup_logger(level=logging.DEBUG):
     stream = logging.StreamHandler(sys.stdout)
     stream.setFormatter(fmt)
     stream.setLevel(level)
+    log.setLevel(logging.DEBUG)
     log.addHandler(stream)
     return log
 
