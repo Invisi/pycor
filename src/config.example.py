2fb9381ab6a733e156e7a399dc7ad25a8b193aaf
Yannick Linke <invisi@0x0f.net>
2019-01-28 10:51:35 +0100

    Updated example config

--- src/config.example.py
+++ src/config.example.py
@@ -1,20 +1,26 @@
+# Sets console output level, default is INFO.
+DEBUG = False
+
 # Sets folders containing subjects over which will be iterated
 FOLDERS = [
     'test\\folder 1',
     'test\\folder 2'
 ]
 
-# Sets console output level, default is INFO.
-DEBUG = False
+# Username
+MAIL_USER = "root@localhost"
+# Password
+MAIL_PASS = "root"
+# IMAP server
+MAIL_IMAP = "mail.fh-aachen.de"
+# SMTP server
+MAIL_SMTP = "mail.fh-aachen.de:465"
 
 # Disable mail sending
 DISABLE_OUTGOING_MAIL = True
 
 # Whether to flag mails as read
 MARK_MAILS_AS_READ = False
-
-# Make excel visible during processing
-SHOW_EXCEL = False
 
 # Delay (in s) between mailbox logins
 DELAY_MAILBOXES = 15
@@ -27,6 +33,9 @@ PSW_PASSPHRASE = 'gC9VGy09lEk7zK1257Pzj5-mDPclX_FScqLC2RLObyU='
 
 # Ignored folder names
 FOLDER_IGNORE = ['_postprocessing', 'PyCor_documentation']
+
+# Make excel visible during processing
+SHOW_EXCEL = False
 
 # Sentry DSN
 SENTRY_DSN = None
